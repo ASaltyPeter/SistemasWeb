426d6756a7bb49a3d08bf46c3f03984722f7ff8f
NicollyDC <nicollydias@ucl.br>
2025-10-12 12:55:20 -0300

    Update lambda_simulation.py

--- trabalho/simulations/lambda_simulation.py
+++ trabalho/simulations/lambda_simulation.py
@@ -1,9 +1,10 @@
 from sqs_simulation import receber_mensagens
 
-def lambda_handler():
+def lambda_handler(event=None, context=None):
     mensagens = receber_mensagens()
     for msg in mensagens:
         print(f"Lambda processou: {msg}")
+    return {"statusCode": 200, "body": "Mensagens processadas"}
 
 if __name__ == "__main__":
     lambda_handler()
