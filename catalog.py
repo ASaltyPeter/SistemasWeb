b0ed6d5637813b712b68fc4bed182ba7a5dfeb50
uiraseb <uiraseeberger@ucl.br>
2025-10-13 22:37:33 -0300

    update delete from main

--- catalog.py
+++
@@ -1,5 +0,0 @@
-from typing import Dict, Any
-
-
-def make_catalog_item(item_id: str, title: str, genre: str) -> Dict[str, Any]:
-    return {"id": item_id, "title": title, "genre": genre}
--- user_views.py
+++
@@ -1,5 +0,0 @@
-from typing import Dict, Any
-
-
-def make_view(user_id: str, item_id: str) -> Dict[str, Any]:
-    return {"userId": user_id, "itemId": item_id}
